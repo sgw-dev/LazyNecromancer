3d87e4fcea793385d7a10a7b702884d82c3993e1
goyafuso <goyafuso.voided@gmail.com>
2021-10-13 18:43:12 -0400

    added folder

---
+++ LazyNecromancer/Assets/Scripts/Rats/BoidController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BoidController : MonoBehaviour {
+    
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+
+    }
+}
