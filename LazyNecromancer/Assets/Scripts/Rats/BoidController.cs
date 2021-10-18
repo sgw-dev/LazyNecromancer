b89196506951665674f26e2b9b23d4bc73a5fbdb
goyafuso <goyafuso.voided@gmail.com>
2021-10-18 13:30:52 -0400

    working not optimized boids

--- LazyNecromancer/Assets/Scripts/Rats/BoidController.cs
+++ LazyNecromancer/Assets/Scripts/Rats/BoidController.cs
@@ -1,15 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 /*
-class Boid {
-    public Vector3 position;
-    public Vector3 velocity;
-    public Vector3 rule1;
-    public Vector3 rule2;
-    public Vector3 rule3;
-}
-*/
+    Unoptimized boids
+ */
 public class BoidController : MonoBehaviour {
     
     public int numberOfBoids;
@@ -18,15 +13,30 @@ public class BoidController : MonoBehaviour {
     public int Ymax;
     public int Ymin;
 
+    public float boidSpeed;
+
 
     
-    public float boidSpeed;
     ///////////
     public Sprite boidprefab;
+    bool followMouse = true;
     
+
+
+
     List<Boid> boids;
     static readonly float FACTOR = 100;
+    static readonly float FACTOR3 = 10;
     public float Separation = 3;
+    private Vector3 target;
+    
+
+    public  float behaviourTimerMax;
+    private float behaviourTimer;
+    public  float swapvalue;
+    private float swappedvalue;
+    //debugging
+    public Text debugger;
     
     public void CreateBoids() {
         boids = new List<Boid>();
@@ -47,33 +57,81 @@ public class BoidController : MonoBehaviour {
     }
 
     void Start() {
+        behaviourTimer = 0f;
         CreateBoids();
         //Debug.Break();
     }
 
+    //Debugging purposues only
     int counter = 0;
+    //Debugging purposues only
+
     void Update() {
+
+        target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        BehaviourChange();
         MoveBoids();
+
+        //Debugging only
         if(counter > 100) {
             //Debug.Break();
             counter = 0;
         }
         counter ++;
+        //Debugging purposues only
+
     }
 
+
+    void BehaviourChange() {
+        behaviourTimer+=Time.deltaTime;
+        if(behaviourTimer >= behaviourTimerMax) {
+            behaviourTimer=0f;
+            swappedvalue = Separation;
+            Separation = swapvalue;
+            swapvalue = swappedvalue;
+        }
+    }
+        
+
+    void OutOfBoundsBreak(Boid b) {
+        if(b.transform.position.x > Xmax || b.transform.position.x < Xmin) {
+                Debug.Break();
+            }
+            if(b.transform.position.y > Ymax || b.transform.position.y < Ymin) {
+                Debug.Break();
+        }
+    }
     void MoveBoids() {
         foreach(Boid b in boids) {
             Vector3 v1 = Rule1(b);
             Vector3 v2 = Rule2(b);
             Vector3 v3 = Rule3(b);
             Vector3 bp = BoundPosition(b);
-            b.velocity = b.velocity + v1 + v2 +v3 + bp;// + more stuff
+            Vector3 t = Target(b);
+            b.velocity = b.velocity + v1 + v2 + v3 + bp + (followMouse?t:Vector3.zero);// + more stuff
+            //b.velocity  = b.velocity + t;
+            debugger.text = "V1 = " + v1 + "\n" +
+                            "V2 = " + v2 + "\n" +
+                            "V3 = " + v3 + "\n" +
+                            "bp = " + bp + "\n" +
+                            "t  = " + t +"";
             if(b.velocity.magnitude > 5) {
                 b.velocity= (b.velocity / b.velocity.magnitude) * boidSpeed;
             }
-            //b.velocity = b.velocity.normalized;
-            //b.position = b.position + b.velocity
+
+            //Debugging only
+            // OutOfBoundsBreak(b);
+            //Debugging only
+        
         }
+    }
+
+    //Go toward
+    Vector3 Target(Boid boid) {
+        Vector3 dir = target- boid.transform.position;
+        dir.z=0f;
+        return dir.normalized;
     }
 
     //cohesion
@@ -137,5 +195,9 @@ public class BoidController : MonoBehaviour {
         }
         
         return v;
+    }
+
+    public void ToggleFollow() {
+        followMouse = ! followMouse;
     }
 }
