a5202a7bf11f993fa762f1f4dc5c14fa1195108f
goyafuso <goyafuso.voided@gmail.com>
2021-12-02 21:02:41 -0500

    SmallSlimes Move

--- LazyNecromancer/Assets/Scripts/Slime/SlimeProjectile.cs
+++ LazyNecromancer/Assets/Scripts/Slime/SlimeProjectile.cs
@@ -5,35 +5,38 @@ using UnityEngine;
 
 public class SlimeProjectile : MonoBehaviour {
 
-    public AnimationCurve travelarc;
     [SerializeField]
-    float TimeUntilImpact;
-    float timer;
+    protected AnimationCurve travelarc;
+    [SerializeField]
+    protected float TimeUntilImpact;
+    protected float timer;
 
-    public float maxHeight; 
+    [SerializeField]
+    protected float maxHeight; 
     
-    Animator anim;
+    protected Animator anim;
 
     public delegate void ReturnToPool(SlimeProjectile glob);
     public ReturnToPool putback;
     
-    GameObject inRange;
 
-    bool dmgDealt;
+    protected GameObject inRange;
 
-    void Awake() {
+    protected bool dmgDealt;
+
+    private protected void Awake() {
         anim = GetComponent<Animator>();
     }
 
-    void Start() {
+    private protected void Start() {
         Reset();
     }
 
-    void OnEnable() {
+    private protected void OnEnable() {
         Reset();
     }
 
-    void Update() {
+    private protected void Update() {
         // if(Input.GetKeyDown(KeyCode.Alpha2)) {
         //     Reset();
         //     Lob(transform.position, GameObject.Find("Player Sprite").transform.position);
@@ -44,17 +47,17 @@ public class SlimeProjectile : MonoBehaviour {
         // }
     }
 
-    public void Done() {
+    private protected void Done() {
         putback(this);
     }
 
-    public void Reset() {
+    private protected void Reset() {
         dmgDealt = false;
         timer = 0f;
         anim.SetBool("Flying",true);
     }
 
-    public void ThrowProjectile(Vector3 start,Vector3 end) {
+    public virtual void ThrowProjectile(Vector3 start,Vector3 end) {
         
         if(Random.value > .5) {
             //Task.Run(async () => await Lob(start,end));
@@ -66,7 +69,7 @@ public class SlimeProjectile : MonoBehaviour {
     }
 
     bool _animating;
-    async Task Lob(Vector3 start,Vector3 end) {
+    private protected async Task Lob(Vector3 start,Vector3 end) {
         if(!_animating) {
             _animating=true;
             while(timer < TimeUntilImpact) {
@@ -83,7 +86,7 @@ public class SlimeProjectile : MonoBehaviour {
         }
     }
 
-    async Task Hurl(Vector3 start,Vector3 end) {
+    private protected async Task Hurl(Vector3 start,Vector3 end) {
         if(!_animating) {
             _animating=true;
             while(timer < TimeUntilImpact) {
@@ -97,11 +100,11 @@ public class SlimeProjectile : MonoBehaviour {
         }
     }
 
-    public void Impact() {
+    public virtual void Impact() {
         anim.SetBool("Flying",false);
     }
 
-    void OnTriggerEnter2D(Collider2D col) {
+    private protected void OnTriggerEnter2D(Collider2D col) {
         if(!dmgDealt && col.CompareTag("Player")) {
             dmgDealt=true;
             Debug.Log("Player was hit by a glob!");
--- LazyNecromancer/Assets/Scripts/Slime/SmallSlimeController.cs
+++ LazyNecromancer/Assets/Scripts/Slime/SmallSlimeController.cs
@@ -2,25 +2,69 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SmallSlimeController : MonoBehaviour {
+public class SmallSlimeController : SlimeProjectile {
     
-    public AnimationCurve travelarc;
     [SerializeField]
-    float TimeUntilImpact;
-    float timer;
+    int MaxHealth;
+    int health;
 
-    public float maxHeight; 
-    
-    Animator anim;
+    public int Damage;
 
-    public delegate void ReturnToPool(SlimeProjectile glob);
-    public ReturnToPool putback;
-    
-    GameObject inRange;
-    bool dmgDealt;
+    public float maxTravelDist;
 
-    void Awake() {
-        anim = GetComponent<Animator>();
+    public Transform target;
+
+
+    //hide from base class
+    new void Awake() {
+        base.Awake();
+        
     }
 
+    //hide start from base class
+    void Start() {
+        
+        target = GameObject.Find("Player Sprite").transform;
+        canMoveAgain=true;
+    }
+
+    //hide reset from base class
+    void OnEnable() {
+
+    }
+
+
+    bool canMoveAgain;
+    public void Move() {
+        if(canMoveAgain) {
+            timer=0f;
+            canMoveAgain=false;
+            //get distance
+            //make sure can only move x amount
+            Vector3 direction = (target.position - transform.position).normalized;
+            float dist = Vector3.Distance(transform.position,target.position);
+            float percent = (maxTravelDist/dist);
+            
+            if( dist < maxTravelDist) {
+                Lob(transform.position,target.position);
+            } else {
+                Lob(transform.position,Vector3.Lerp(transform.position,target.position,percent));
+            }
+        }
+
+    }
+
+    
+    public override void Impact() {
+        anim.SetTrigger("Impact");
+        canMoveAgain=true;
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D col) {
+        if(!dmgDealt && col.CompareTag("Player")) {
+            Debug.Log("Player was hit by a small slime!");
+        }
+    }
 }
