842483ad29d345c002c263422eb8a6bc6712611d
dthomp69@emich.edu <dthomp69@emich.edu>
2022-02-07 20:33:41 -0500

    Got objects set up in the C# script

---
+++ LazyNecromancer/Assets/Scripts/LevelUpScreen/LevelUpScreen.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelUpScreen : MonoBehaviour
+{
+    public Button HealthUp;
+    public Button HealthDown;
+    public Button ManaUp;
+    public Button ManaDown;
+    public Button StrengthUp;
+    public Button StrengthDown;
+    public Button MagicUp;
+    public Button MagicDown;
+    public Button StaminaUp;
+    public Button StaminaDown;
+    public Text HealthCost;
+    public Text ManaCost;
+    public Text StrengthCost;
+    public Text MagicCost;
+    public Text StaminaCost;
+    public Text Health;
+    public Text Mana;
+    public Text Strength;
+    public Text Magic;
+    public Text Stamina;
+    public Text EXP;
+    public Button Keep;
+    public Button Revert;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
