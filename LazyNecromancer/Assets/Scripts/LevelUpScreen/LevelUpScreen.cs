843ae788c981ef2a85b3273c1e16f93418b0ce13
dthomp69@emich.edu <dthomp69@emich.edu>
2022-03-18 14:08:39 -0400

    updated local files

--- LazyNecromancer/Assets/Scripts/LevelUpScreen/LevelUpScreen.cs
+++ LazyNecromancer/Assets/Scripts/LevelUpScreen/LevelUpScreen.cs
@@ -29,10 +29,48 @@ public class LevelUpScreen : MonoBehaviour
     public Button Keep;
     public Button Revert;
 
+
+    //Will need a method to take these values in from the player
+    public int healthValue;
+    public int manaValue;
+    public int strengthValue;
+    public int magicValue;
+    public int staminaValue;
+    public int expValue;
+
+    public int adjustedHealthValue;
+    public int adjustedManaValue;
+    public int adjustedStrengthValue;
+    public int adjustedMagicValue;
+    public int adjustedStaminaValue;
+    public int adjustedExpValue;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        healthValue = 10;
+        manaValue = 10;
+        strengthValue = 10;
+        magicValue = 10;
+        staminaValue = 10;
+
+        this.Health.text = healthValue.ToString();
+        this.Mana.text = manaValue.ToString();
+        this.Strength.text = strengthValue.ToString();
+        this.Magic.text = magicValue.ToString();
+        this.Stamina.text = staminaValue.ToString();
+
+        expValue = 1000;
+
+        this.EXP.text = "EXP: " + expValue.ToString();
+
+        //TODO: Will need functions to calculate these, placeholder
+        this.HealthCost.text = "To level: " + 100;
+        this.ManaCost.text = "To level: " + 100;
+        this.StrengthCost.text = "To level: " + 100;
+        this.MagicCost.text = "To level: " + 100;
+        this.StaminaCost.text = "To level: " + 100;
+
     }
 
     // Update is called once per frame
